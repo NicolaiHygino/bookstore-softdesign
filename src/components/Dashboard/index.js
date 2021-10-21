b8866dac0e1b120c4dce053e58c392b9ce594780
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-10-21 13:10:33 -0300

    feat (Dashboard): Dashboard now have Grid books list

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,8 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { 
+  Header,
+  Grid,
+  GridItem,
+  Image,
+  InfoWrapper,
+  Title,
+  Price,
+  ButtonWrapper,
+  Button,
+} from './style';
 
 const Dashboard = () => {
   return (
-    <h1>Dashboard</h1>
+    <>
+      <Header>
+        <h1>Bookstore</h1>
+      </Header>
+      <Grid>
+        <GridItem>
+          <Image />
+          <InfoWrapper>
+            <Title>This is a Book</Title>
+            <Price>$8.99</Price>
+            <ButtonWrapper>
+              <Button>Rent</Button>
+              <Button>Details</Button>
+            </ButtonWrapper>
+          </InfoWrapper>
+        </GridItem>
+        <GridItem>
+          <Image />
+          <InfoWrapper>
+            <Title>This is a Book</Title>
+            <Price>$8.99</Price>
+            <ButtonWrapper>
+              <Button>Rent</Button>
+              <Button>Details</Button>
+            </ButtonWrapper>
+          </InfoWrapper>
+        </GridItem>
+        <GridItem>
+          <Image />
+          <InfoWrapper>
+            <Title>This is a Book</Title>
+            <Price>$8.99</Price>
+            <ButtonWrapper>
+              <Button>Rent</Button>
+              <Button>Details</Button>
+            </ButtonWrapper>
+          </InfoWrapper>
+        </GridItem>
+        <GridItem>
+          <Image />
+          <InfoWrapper>
+            <Title>This is a Book</Title>
+            <Price>$8.99</Price>
+            <ButtonWrapper>
+              <Button>Rent</Button>
+              <Button>Details</Button>
+            </ButtonWrapper>
+          </InfoWrapper>
+        </GridItem>
+      </Grid>
+    </>
   );
 };
 
--- src/components/Dashboard/style.js
+++ src/components/Dashboard/style.js
@@ -1,1 +1,59 @@
 import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 10px 20px;
+  height: 60px;
+  background-color: #ecf0f1;
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  max-width: 1000px;
+  padding: 10px;
+  margin: 30px auto;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 20px;
+`;
+
+export const GridItem = styled.div`
+  border: 1px solid #ecf0f1;
+  border-radius: 5px;
+  overflow: hidden;
+`;
+
+export const Image = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: palevioletred;
+`;
+
+export const InfoWrapper = styled.div`
+  padding: 15px;
+`;
+
+export const Title = styled.h2`
+  font-size: 1.3em;
+  font-weight: 800;
+`;
+
+export const Price = styled.p`
+  font-size: 1em;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+`;
+
+export const Button = styled.button`
+  background-color: var(--main-color);
+  border: 0;
+  border-radius: 3px;
+  color: white;
+  padding: .6em 1em;
+  cursor: pointer;
+`;
