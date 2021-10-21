673c90dc61b8fadeb04af777a449ecab79d3c86d
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-10-21 16:22:13 -0300

    feat (Dashboard): Filter books by title

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import * as S from './style';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 const Dashboard = () => {
   const [books, setBooks] = useState([]);
-  
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredBooks = books
+    .filter(book => book.title.toLowerCase().includes(searchTerm));
+
   useEffect(() => {
     axios.get('books.json').then(({data}) => setBooks(data));
   }, []);
@@ -15,21 +20,36 @@ const Dashboard = () => {
         <h1>Bookstore</h1>
       </S.Header>
 
-      <S.Grid>
-        {books.map(book => (
-          <S.GridItem key={book.author + book.title}>
-            <S.Image src={require(`assets/images/books/${book.image}`).default} />
-            <S.InfoWrapper>
-              <S.Title>{book.title}</S.Title>
-              <S.Price>$8.99</S.Price>
-              <S.ButtonWrapper>
-                <S.Button>Rent</S.Button>
-                <S.Button>Details</S.Button>
-              </S.ButtonWrapper>
-            </S.InfoWrapper>
-          </S.GridItem>
-        ))}
-      </S.Grid>
+      <S.Content>
+        <label htmlFor="search">Search </label>
+        <S.IconField>
+          <AiOutlineSearch size="1.5em" />
+          <S.Input
+            id="search"
+            type="text"
+            name="search"
+            value={searchTerm} 
+            onChange={e => setSearchTerm(e.target.value.toLowerCase())}
+          />
+        </S.IconField>
+        
+
+        <S.Grid>
+          {filteredBooks.map(book => (
+            <S.GridItem key={book.author + book.title}>
+              <S.Image src={require(`assets/images/books/${book.image}`).default} />
+              <S.InfoWrapper>
+                <S.Title>{book.title}</S.Title>
+                <S.Price>$8.99</S.Price>
+                <S.ButtonWrapper>
+                  <S.Button>Rent</S.Button>
+                  <S.Button>Details</S.Button>
+                </S.ButtonWrapper>
+              </S.InfoWrapper>
+            </S.GridItem>
+          ))}
+        </S.Grid>
+      </S.Content>
     </>
   );
 };
--- src/components/Dashboard/style.js
+++ src/components/Dashboard/style.js
@@ -9,11 +9,33 @@ export const Header = styled.header`
   background-color: #ecf0f1;
 `;
 
+export const Content = styled.section`
+  margin: 30px auto;  
+  max-width: 1000px;
+`;
+
+export const IconField = styled.div`
+  display: flex;
+  align-items: center;  
+  position: relative;
+
+  & svg {
+    position: absolute;
+    margin: 0 .5em;
+    left: 0;
+  }
+`;
+
+export const Input = styled.input`
+  width: 300px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: .8em 1em .8em 3em;
+`;
+
 export const Grid = styled.div`
   display: grid;
-  max-width: 1000px;
   padding: 10px;
-  margin: 30px auto;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 30px;
 `;
